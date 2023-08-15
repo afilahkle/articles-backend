a8711c90b1524e67c1946354beb59cbec60ed735
Omar Khalifa <elkhalifa002@gmail.com>
2023-08-15 12:11:30 +0300

    configure postgres database connection and add .env.example file

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,31 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TagModule } from './tag/tag.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [TagModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        // entities: [],
+        autoLoadEntities: true, // don't add this in production.
+        synchronize: true, // don't add this in production. 
+      }),
+    }),
+    TagModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
