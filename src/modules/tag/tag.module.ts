decd6d0a879daaa257702a11e80b65366b3f9566
Omar Khalifa <elkhalifa002@gmail.com>
2023-08-17 09:50:58 +0300

    add TagEntity to imports in tag.module

--- src/modules/tag/tag.module.ts
+++ src/modules/tag/tag.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TagController } from './tag.controller';
 import { TagService } from './tag.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TagEntity } from './entities/tag.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([TagEntity]),
+  ],
   controllers: [TagController],
   providers: [TagService]
 })
