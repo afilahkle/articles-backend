c217693ccbce710897ff0945de167f50e0548bd5
Omar Khalifa <elkhalifa002@gmail.com>
2023-08-18 15:47:27 +0300

    update user service to save the user into the  database

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dtos/createUser.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from './entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UserService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>
+  ) {
+
+  }
   async createUser(createUserDto: CreateUserDto) {
-    return createUserDto;
+    const newUser = new UserEntity();
+    Object.assign(newUser, createUserDto);
+    return this.userRepository.save(newUser);
   }
 }
