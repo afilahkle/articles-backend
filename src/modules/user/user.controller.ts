3928ce095761c13e0843244a3e77a979cf2d1a9d
Omar Khalifa <elkhalifa002@gmail.com>
2023-08-18 11:41:41 +0300

    inject UserService inside UserController and verify its working correctly

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,9 +1,15 @@
 import { Controller, Post } from '@nestjs/common';
+import { UserService } from './user.service';
 
 @Controller('api')
 export class UserController {
+  constructor(
+    private readonly userService: UserService
+  ) {
+
+  }
   @Post("users")
   async createUser (): Promise<any> {
-    return 'createUser';
+    return this.userService.createUser();
   }
 }
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  async createUser() {
+    return "createUser form service";
+  }
+}
