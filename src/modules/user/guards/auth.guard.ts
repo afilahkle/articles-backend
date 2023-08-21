42a5294b6b4fa579a5f7a9f008d0c7af9dfe6f68
Omar Khalifa <elkhalifa002@gmail.com>
2023-08-21 12:56:34 +0300

    implement auth guard to handle the case if a user exists or not

---
+++ src/modules/user/guards/auth.guard.ts
@@ -0,0 +1,16 @@
+import { CanActivate, ExecutionContext, HttpException, HttpStatus, Injectable } from "@nestjs/common";
+import { Observable } from "rxjs";
+import { ExpressRequest } from "src/types/expressRequest.interface.";
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+      const request = context.switchToHttp().getRequest<ExpressRequest>()
+
+      if (request.user) {
+        return true;
+      }
+
+      throw new HttpException("Not Authorized", HttpStatus.UNAUTHORIZED)
+  }
+}
