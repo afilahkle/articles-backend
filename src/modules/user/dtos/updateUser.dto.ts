6d4e3ecf021b9e33bd47e435779909d0d2eb925c
Omar Khalifa <elkhalifa002@gmail.com>
2023-08-21 15:01:45 +0300

    create a dto for update user

---
+++ src/modules/user/dtos/updateUser.dto.ts
@@ -0,0 +1,12 @@
+import { IsEmail } from "class-validator";
+
+export class UpdateUserDto {
+  readonly username: string;
+
+  @IsEmail()
+  readonly email: string;
+
+  readonly bio: string;
+
+  readonly image: string;
+}
