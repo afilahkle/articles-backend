8573056a50cb33e4310c104af9b2cd3bc48ed2f6
Omar Khalifa <elkhalifa002@gmail.com>
2023-08-22 14:47:48 +0300

    create a post route api/articles for testing

--- src/modules/article/article.controller.ts
+++ src/modules/article/article.controller.ts
@@ -1,4 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 
 @Controller('api/articles')
-export class ArticleController {}
+export class ArticleController {
+  @Post()
+  async createArticle() {
+    return "create article";
+  }
+}
