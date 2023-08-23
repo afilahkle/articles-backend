d3e88c98a11ad6169fa55190a73ba025c928e4f3
Omar Khalifa <elkhalifa002@gmail.com>
2023-08-23 15:41:43 +0300

    implement the functionality of createArticle method

--- src/modules/article/article.controller.ts
+++ src/modules/article/article.controller.ts
@@ -1,6 +1,9 @@
-import { Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '../user/guards/auth.guard';
+import { User } from '../user/decorators/user.decorator';
+import { CreateArticleDto } from './dtos/createArticle.dto';
 import { ArticleService } from './article.service';
+import { UserEntity } from '../user/entities/user.entity';
 
 @Controller('api/articles')
 export class ArticleController {
@@ -11,7 +14,7 @@ export class ArticleController {
 
   @Post()
   @UseGuards(AuthGuard)
-  async create(): Promise<any> {
-    return await this.articleService.createArticle()
+  async create(@User() currentUser: UserEntity, @Body('article') createArticleDto:CreateArticleDto): Promise<any> {
+    return await this.articleService.createArticle(currentUser, createArticleDto)
   }
 }
--- src/modules/article/article.service.ts
+++ src/modules/article/article.service.ts
@@ -1,8 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { CreateArticleDto } from './dtos/createArticle.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ArticleEntity } from './entities/article.entity';
+import { Repository } from 'typeorm';
+import { UserEntity } from '../user/entities/user.entity';
 
 @Injectable()
 export class ArticleService {
-  async createArticle () {
-    return "create article from serivce";
+  constructor(
+    @InjectRepository(ArticleEntity)
+    private readonly articleRepository: Repository<ArticleEntity>
+  ){
+  }
+
+  async createArticle (currentUser: UserEntity, createArticleDto: CreateArticleDto): Promise<any> {
+    const article = new ArticleEntity();
+    Object.assign(article, createArticleDto)
+
+    if (!article.tagList) {
+      article.tagList = []
+    }
+
+    article.slug = 'foo'; // just for testing
+
+    article.author = currentUser;
+    
+    console.log(article);
+    return await this.articleRepository.save(article);
+  
   }
 }
