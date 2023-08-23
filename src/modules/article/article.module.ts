bd5d6e4d3c2ba72e3f8206bf9db70018cfd7993f
Omar Khalifa <elkhalifa002@gmail.com>
2023-08-23 14:55:03 +0300

    register TypeOrmModule for ArticleEntity inside article module

--- src/modules/article/article.module.ts
+++ src/modules/article/article.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ArticleService } from './article.service';
 import { ArticleController } from './article.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ArticleEntity } from './entities/article.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([ArticleEntity])
+  ],
   providers: [ArticleService],
   controllers: [ArticleController]
 })
