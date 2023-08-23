45e55ccd680d7b05dc41b9834cb937ac108757ca
Omar Khalifa <elkhalifa002@gmail.com>
2023-08-23 12:29:14 +0300

    add authentication required to create action

--- src/modules/article/article.controller.ts
+++ src/modules/article/article.controller.ts
@@ -1,8 +1,10 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Post, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '../user/guards/auth.guard';
 
 @Controller('api/articles')
 export class ArticleController {
   @Post()
+  @UseGuards(AuthGuard)
   async create() {
     return "create article";
   }
