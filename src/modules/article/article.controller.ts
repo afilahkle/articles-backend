9e968221e992de87d8cb357d2afbbfcd03c2c1c1
Omar Khalifa <elkhalifa002@gmail.com>
2023-08-23 12:44:31 +0300

    inject article service

--- src/modules/article/article.controller.ts
+++ src/modules/article/article.controller.ts
@@ -1,11 +1,17 @@
 import { Controller, Post, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '../user/guards/auth.guard';
+import { ArticleService } from './article.service';
 
 @Controller('api/articles')
 export class ArticleController {
+  constructor(
+    private readonly articleService: ArticleService
+  ){
+  }
+
   @Post()
   @UseGuards(AuthGuard)
-  async create() {
-    return "create article";
+  async create(): Promise<any> {
+    return await this.articleService.createArticle()
   }
 }
