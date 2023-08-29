f1d8de7e01b4ef49574aa60c4089eab2a14672cc
Omar Khalifa <elkhalifa002@gmail.com>
2023-08-29 21:40:56 +0300

    inject follow entity

--- src/modules/article/article.module.ts
+++ src/modules/article/article.module.ts
@@ -4,10 +4,11 @@ import { ArticleController } from './article.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ArticleEntity } from './entities/article.entity';
 import { UserEntity } from '../user/entities/user.entity';
+import { FollowEntity } from '../profile/entities/follow.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([ArticleEntity, UserEntity])
+    TypeOrmModule.forFeature([ArticleEntity, UserEntity, FollowEntity])
   ],
   providers: [ArticleService],
   controllers: [ArticleController]
