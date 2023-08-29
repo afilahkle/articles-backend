e887bf0496ec483e318f96ac5c33ac57f8c9b784
Omar Khalifa <elkhalifa002@gmail.com>
2023-08-29 12:16:23 +0300

    create profile service

--- src/modules/profile/profile.module.ts
+++ src/modules/profile/profile.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ProfileController } from './profile.controller';
+import { ProfileService } from './profile.service';
 
 @Module({
-  controllers: [ProfileController]
+  controllers: [ProfileController],
+  providers: [ProfileService]
 })
 export class ProfileModule {}
