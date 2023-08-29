19e6734bb21dd87ca843601dc7096e3c2415ebb5
Omar Khalifa <elkhalifa002@gmail.com>
2023-08-29 17:48:44 +0300

    create a get profile (api/profiles/:username) route

--- src/modules/profile/profile.controller.ts
+++ src/modules/profile/profile.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { ProfileResponseInterface } from './types/profileResponse.interface';
+import { User } from '../user/decorators/user.decorator';
+import { ProfileService } from './profile.service';
+import { ProfileType } from './types/profile.type';
 
-@Controller('profile')
-export class ProfileController {}
+@Controller('api/profiles')
+export class ProfileController {
+  constructor(
+    private readonly profileService: ProfileService
+  ) {
+  }
+
+  @Get(':username')
+  async getProfile(
+    @User('id') currentUserId: number,
+    @Param('username') profileUsername: string
+  ): Promise<ProfileResponseInterface> {
+    const profile = await this.profileService.getProfile(currentUserId, profileUsername);
+    return this.profileService.buildProfileResponse(profile);
+  } 
+    
+}
--- src/modules/profile/profile.service.ts
+++ src/modules/profile/profile.service.ts
@@ -1,4 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { UserEntity } from '../user/entities/user.entity';
+import { ProfileResponseInterface } from './types/profileResponse.interface';
+import { ProfileType } from './types/profile.type';
+import { Http2ServerRequest } from 'http2';
 
 @Injectable()
-export class ProfileService {}
+export class ProfileService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>
+  ) {
+  }
+
+  async getProfile(currentUserId: number, profileUsername: string): Promise<ProfileType> {
+    const user = await this.userRepository.findOne({
+      where: {
+        username: profileUsername
+      }
+    });
+
+    if (!user) {
+      throw new HttpException('Profile does not exit', HttpStatus.NOT_FOUND)
+    }
+
+    return {
+      ...user,
+      following: false
+    }
+  }
+
+  buildProfileResponse(profile: ProfileType): ProfileResponseInterface {
+    delete profile.email;
+    delete profile.password;
+
+    return {profile}
+  }
+}
