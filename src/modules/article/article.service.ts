2328bddc64f2d2994822bdbfb7e440bc155ffc51
Omar Khalifa <elkhalifa002@gmail.com>
2023-08-29 22:13:26 +0300

    implement Feed route

--- src/modules/article/article.service.ts
+++ src/modules/article/article.service.ts
@@ -7,6 +7,7 @@ import { UserEntity } from '../user/entities/user.entity';
 import { ArticleResponseInterface } from './types/articleResponse.interface';
 import slugify from 'slugify';
 import { ArticlesResponseInterface } from './types/articlesResponse.interface';
+import { FollowEntity } from '../profile/entities/follow.entity';
 
 @Injectable()
 export class ArticleService {
@@ -15,6 +16,8 @@ export class ArticleService {
     private readonly articleRepository: Repository<ArticleEntity>,
     @InjectRepository(UserEntity)
     private readonly userRepository: Repository<UserEntity>,
+    @InjectRepository(FollowEntity)
+    private readonly followRepository: Repository<FollowEntity>,
     private readonly entityManager: EntityManager
   ){
   }
@@ -89,6 +92,46 @@ export class ArticleService {
     })
 
     return {articles: articlesWithFavorited, articlesCount}
+  }
+
+  async getFeed(currentUserId: number, query: any): Promise<ArticlesResponseInterface> {
+    const follows = await this.followRepository.find({
+      where: {
+        followerId: currentUserId
+      }
+    });
+    
+    if (follows.length === 0) {
+      return {
+        articles: [],
+        articlesCount: 0
+      }
+    }
+
+    const followingUserIds = follows.map(follow => follow.followingId);
+    const queryBuilder = this.entityManager.getRepository(ArticleEntity)
+      .createQueryBuilder('articles')
+      .leftJoinAndSelect('articles.author', 'author')
+      .where('articles.authorId IN (:...ids)', {ids: followingUserIds});
+
+    queryBuilder.orderBy('articles.createdAt', 'DESC');
+
+    const articlesCount = await queryBuilder.getCount();
+
+    if (query.limit) {
+      queryBuilder.limit(query.limit);
+    }
+
+    if (query.offset) {
+      queryBuilder.offset(query.offset);
+    }
+
+    const articles = await queryBuilder.getMany();
+    
+    return {
+      articles,
+      articlesCount
+    }
   }
 
   async createArticle (currentUser: UserEntity, createArticleDto: CreateArticleDto): Promise<ArticleEntity> {
