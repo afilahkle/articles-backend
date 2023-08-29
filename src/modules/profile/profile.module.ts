bce60b855cc0bed3faeca5944f3790230f0a6998
Omar Khalifa <elkhalifa002@gmail.com>
2023-08-29 17:40:16 +0300

    inject user entity repository inside profile module

--- src/modules/profile/profile.module.ts
+++ src/modules/profile/profile.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ProfileController } from './profile.controller';
 import { ProfileService } from './profile.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from '../user/entities/user.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([UserEntity])
+  ],
   controllers: [ProfileController],
   providers: [ProfileService]
 })
