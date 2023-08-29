b1c7beaed19c78ef6a2cb679a9779f9a38ca01e9
Omar Khalifa <elkhalifa002@gmail.com>
2023-08-29 17:43:00 +0300

    inject FollowEntity in profile module

--- src/modules/profile/profile.module.ts
+++ src/modules/profile/profile.module.ts
@@ -3,10 +3,11 @@ import { ProfileController } from './profile.controller';
 import { ProfileService } from './profile.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from '../user/entities/user.entity';
+import { FollowEntity } from './entities/follow.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([UserEntity])
+    TypeOrmModule.forFeature([UserEntity, FollowEntity])
   ],
   controllers: [ProfileController],
   providers: [ProfileService]
