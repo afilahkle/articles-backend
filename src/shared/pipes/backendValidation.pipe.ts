282834ac8ad9692e61603a2ecdb085d73c89ccc1
Omar Khalifa <elkhalifa002@gmail.com>
2023-08-30 15:57:55 +0300

    fix validation of updating user

--- src/shared/pipes/backendValidation.pipe.ts
+++ src/shared/pipes/backendValidation.pipe.ts
@@ -6,6 +6,11 @@ import { ValidationError, validate } from "class-validator";
 export class BackendValidationPipe implements PipeTransform {
   async transform(value: any, metadata: ArgumentMetadata) {
    const object = plainToClass(metadata.metatype, value);
+
+   if (typeof object !== 'object') {
+    return value;
+   }
+
    const errors = await validate(object);
 
     if (errors.length === 0) {
